01d5954598f19278d3524edf71a9d838c10e5813
Joris Dieltiens <joris.dieltiens@gmail.com>
2021-01-05 13:37:45 +0100

    MyTodo xamarin

---
+++ MyToDo/MyToDo/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+
+    }
+}
